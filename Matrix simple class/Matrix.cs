ad77462e666aa8f84a9cf8a22c51b1787996f097
Hayk Talalyan <talalyanhayk@gmail.com>
2019-04-11 22:49:07 +0400

    Adding a matrix class

---
+++ Matrix simple class/Matrix.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Matrix_simple_class
+{
+    public class Matrix
+    {
+        public double[,] Mat { get; set; }
+        public readonly uint Row;
+        public readonly uint Column;
+
+        /// <summary>
+        /// Matrix class constructor
+        /// </summary>
+        /// <param name="row">Number of rows</param>
+        /// <param name="column">Number of columns</param>
+        public Matrix(uint row, uint column)
+        {
+            this.Mat = new double[row, column];
+            this.Row = row;
+            this.Column = column;
+        }
+
+        /// <summary>
+        /// Deafault constructor
+        /// </summary>
+        public Matrix() { }
+
+
+        /// <summary>
+        /// Checking if the matrix is square
+        /// </summary>
+        /// <returns>If it is, it will return true, if no then false</returns>
+        public Boolean IsSquare()
+        {
+            return (Row == Column);
+        }
+
+        /// <summary>
+        /// Method for matrix generation
+        /// </summary>
+        public void RandomGenerateMatrixElements()
+        {
+            Random rd = new Random();
+            for (int i = 0; i < this.Row; i++)
+            {
+                for (int j = 0; j < this.Column; j++)
+                    this.Mat[i, j] = rd.Next(-30, 31); // ... +rd.NextDouble();
+            }
+        }
+
+        /// <summary>
+        /// Method for printing the matrix
+        /// </summary>
+        public void ShowMatrix()
+        {
+            for (int i = 0; i < this.Row; i++)
+            {
+                for (int j = 0; j < this.Column; j++)
+                    Console.Write(this.Mat[i, j] + "\t");
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Method for finding the maximum 
+        /// </summary>
+        /// <returns></returns>
+        public double Max()
+        {
+            double max = this.Mat[0, 0];
+            for (int i = 0; i < this.Row; i++)
+            {
+                for (int j = 0; j < this.Column; j++)
+                {
+                    if (max < this.Mat[i, j])
+                    {
+                        max = this.Mat[i, j];
+                    }
+                }
+            }
+            return max;
+        }
+
+        /// <summary>
+        /// Method for finding the minimum 
+        /// </summary>
+        /// <returns></returns>
+        public double Min()
+        {
+            double min = this.Mat[0, 0];
+            for (int i = 0; i < this.Row; i++)
+            {
+                for (int j = 0; j < this.Column; j++)
+                {
+                    if (min > this.Mat[i, j])
+                    {
+                        min = this.Mat[i, j];
+                    }
+                }
+            }
+            return min;
+        }
+
+        /// <summary>
+        /// Method for adding matrix with similar rows and columns
+        /// </summary>
+        /// <param name="second">Getting as a parameter another matrix</param>
+        /// <returns>Return the result of adding or null if not possible</returns>
+        public Matrix Add(Matrix second)
+        {
+            if (this.Row == second.Row && this.Column == second.Column)
+            {
+                Matrix result = new Matrix(this.Row, this.Column);
+                for (int i = 0; i < this.Row; i++)
+                {
+                    for (int j = 0; j < this.Column; j++)
+                    {
+                        result.Mat[i, j] = this.Mat[i, j] + second.Mat[i, j];
+                    }
+                }
+                return result;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Method for multiplyig a matrix by a number
+        /// </summary>
+        /// <param name="n">Give a number as a parameter</param>
+        /// <returns>Returns the result of multiplying</returns>
+        public Matrix Multiply(double n)
+        {
+            Matrix result = new Matrix(this.Row, this.Column);
+            for (int i = 0; i < this.Row; i++)
+            {
+                for (int j = 0; j < this.Column; j++)
+                    result.Mat[i, j] = n * this.Mat[i, j];
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Method for multiplying a matrix by another matrix
+        /// </summary>
+        /// <param name="second">Give another matrix as parameter</param>
+        /// <returns>Returns the result of multiplying</returns>
+        public Matrix Multiply(Matrix second)
+        {
+            if (this.Column == second.Row)
+            {
+                Matrix result = new Matrix(this.Row, second.Column);
+                for (int i = 0; i < this.Row; i++)
+                {
+                    for (int j = 0; j < second.Column; j++)
+                    {
+                        for (int k = 0; k < this.Column; k++)
+                        {
+                            result.Mat[i, j] += this.Mat[i, k] * second.Mat[k, j];
+                        }
+                    }
+                }
+                return result;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// A method for matrix inversing
+        /// </summary>
+        /// <returns></returns>
+        public Matrix Inverse()
+        {
+            Matrix result = new Matrix(this.Row, this.Column);
+            for (int i = 0; i < this.Row; i++)
+            {
+                for (int j = 0; j < this.Column; j++)
+                {
+
+
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// A method for matrix transposing
+        /// </summary>
+        /// <returns></returns>
+        public Matrix Transpose()
+        {
+            Matrix result = new Matrix(this.Column, this.Row);
+            for (int i = 0; i < this.Row; i++)
+            {
+                for (int j = 0; j < this.Column; j++)
+                    result.Mat[j, i] = this.Mat[i, j];
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// A method to check if matrix is ortogonal
+        /// </summary>
+        /// <returns></returns>
+        public bool isOrtogonal()
+        {
+            bool ortogonal = true;
+            for (int i = 0; i < this.Row; i++)
+            {
+                for (int j = 0; j < this.Column; j++)
+                {
+                    if (this.Inverse().Mat[i, j] != this.Transpose().Mat[i, j])
+                    {
+                        ortogonal = false;
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return ortogonal;
+        }
+    }
+
+
+}
+
--- Matrix simple class/Program.cs
+++ Matrix simple class/Program.cs
@@ -1,15 +1,115 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Matrix_simple_class
 {
     class Program
     {
+        private static bool isOctogonal;
+
         static void Main(string[] args)
         {
+            uint row, column;
+            bool a, b;
+
+            // Generating first matrix while input sizes are correct
+            do
+            {
+                Console.WriteLine("Enter rows and columns of matrix");
+                Console.Write("Rows=");
+                a = uint.TryParse(Console.ReadLine(), out row);
+                Console.Write("Columns=");
+                b = uint.TryParse(Console.ReadLine(), out column);
+                if (!(a && b) || row == 0 || column == 0)
+                {
+                    Console.WriteLine("not correct input data");
+                }
+            } while (!(a && b) || row == 0 || column == 0);
+
+            Matrix matrix1 = new Matrix(row, column);
+            Console.WriteLine("First random generated matrix");
+            matrix1.RandomGenerateMatrixElements();
+            matrix1.ShowMatrix();
+            Console.Write("Max element:" + matrix1.Max() + "\tMin element:" + matrix1.Min());
+            
+
+            //Generating second matrix 
+            Console.WriteLine("\n--------------------------------------");
+            do
+            {
+                Console.WriteLine("Enter rows and columns of matrix");
+                Console.Write("Rows=");
+                a = uint.TryParse(Console.ReadLine(), out row);
+                Console.Write("Columns=");
+                b = uint.TryParse(Console.ReadLine(), out column);
+                if (!(a && b) || row == 0 || column == 0)
+                {
+                    Console.WriteLine("not correct input data");
+                }
+            } while (!(a && b) || row == 0 || column == 0);
+
+            Matrix matrix2 = new Matrix(row, column);
+            Console.WriteLine("Second random generated matrix");
+            matrix2.RandomGenerateMatrixElements();
+            matrix2.ShowMatrix();
+            Console.Write("Max element:" + matrix2.Max() + "\tMin element:" + matrix2.Min());
+           
+
+            //show how to add two matrixes
+            Console.WriteLine("\n**************************************");
+            if (matrix1.Row == matrix2.Row && matrix1.Column == matrix2.Column)
+            {
+                Console.WriteLine("Addition of two matrixes equals");
+                matrix1.Add(matrix2).ShowMatrix();
+            }
+            else
+            {
+                Console.WriteLine("Impossible to add this matrixes because of different resolution");
+            }
+            
+
+            //Multiply operation of matrix by a number
+            Console.WriteLine("**************************************");
+            Console.Write("Please input a number: n=");
+            double n;
+            a = double.TryParse(Console.ReadLine(), out n);
+            if (a)
+            {
+                Console.WriteLine("Multiply of first matrix by number n is");
+                matrix1.Multiply(n).ShowMatrix();
+            }
+            else
+            {
+                Console.WriteLine("n is not a number");
+            }
+            
+
+            //Multiply operation with two matrixes
+            Console.WriteLine("**************************************");
+            if (matrix1.Column == matrix2.Row)
+            {
+                Console.WriteLine("Multiply of first and second matrixes is");
+                matrix1.Multiply(matrix2).ShowMatrix();
+            }
+            else
+            {
+                Console.WriteLine("Impossible multiply this two matrixes because of resolution");
+            }
+            
+
+            //Transpose first matrix
+            Console.WriteLine("**************************************");
+            Console.WriteLine("Transpose of first matrix equals");
+            matrix1.Transpose().ShowMatrix();
+            Console.ReadKey();
+
+            // Checking if the matrixes ar ortogonal
+            Console.WriteLine("**************************************");
+            if(isOctogonal)
+            {
+                Console.WriteLine("They are octogonal");
+            }
         }
     }
+
 }
+   
